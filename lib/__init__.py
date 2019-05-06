eaa2e8ed9aed5209e67950f2746a2fac19c9d7d0
Unknown <venom@gen-x.co.nz>
2019-05-06 22:18:31 +1200

    Make our libs first to load.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
 import requests
 requests.packages.urllib3.disable_warnings()
